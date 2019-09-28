e0b6657c8062274d5f910afbeb327b4c299e84a0
aplicatii-romanesti <aplicatii.romanesti@gmail.com>
2019-09-28 22:22:49 +0300

    use books with howto incorporated

--- scripts/build_all_molitfelnic_based.sh
+++ scripts/build_all_molitfelnic_based.sh
@@ -1,7 +1,14 @@
-#!/bin/bash -vxeu
+#!/bin/bash -xeuo pipefail
+
+echo "1. Copy the ~/Books_with_HowTO (howto image incorporated) to the ~/Books folder (which we use)"
+cp -rp ~/Books_with_HowTO/* ~/Books/
+
+echo "2. Going to start build one by one:"
 
 for app in BibliaOrtodoxa VietileSfintilor BibliotecaOrtodoxa Pidalion; do
   echo "Building $app"
   git reset --hard HEAD
   ./molitfelnic_to_any_app.sh $app && ./dockerbuild.sh $app
 done
+
+
